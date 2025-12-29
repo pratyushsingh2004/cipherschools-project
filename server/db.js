75573e9bc9d688b57987d65a9ebd9187e9c1ae50
Pratyush Singh <118829094+pratyushsingh2004@users.noreply.github.com>
2025-12-29 18:22:37 +0530

    Create db.js

---
+++ server/db.js
@@ -0,0 +1,19 @@
+const { Pool } = require('pg');
+require('dotenv').config();
+
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+});
+
+pool.on('connect', () => {
+  console.log('✅ PostgreSQL Connected');
+});
+
+module.exports = pool;
